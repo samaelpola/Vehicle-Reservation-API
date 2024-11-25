49c896eae18761c00315956da262a4bdb16601d2
samael pola <samaelpola06@gmail.com>
2024-11-25 15:20:13 +0100

    fix: middleware, clean code

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM node:23.0.0
 
 WORKDIR /app
 
+ADD https://raw.githubusercontent.com/vishnubob/wait-for-it/81b1373f17855a4dc21156cfe1694c31d7d1792e/wait-for-it.sh /usr/bin/wait-for-it
+RUN chmod +x /usr/bin/wait-for-it
+
 COPY package*.json .
 
 RUN npm install
@@ -12,4 +15,4 @@ ENV PORT=3000
 
 EXPOSE ${PORT}
 
-CMD ["npm", "start"]
+CMD ["wait-for-it", "postgres:5432", "--", "npm", "start"]
--- src/middlewares/vehicle.middleware.js
+++ src/middlewares/vehicle.middleware.js
@@ -1,8 +1,8 @@
-const Vehicle = require("../models/vehicle.model");
+const {getVehicle} = require("../services/vehicle.service");
 
 const checkVehicleExist = async (req, res, next) => {
     const { vehicleId } = req.params;
-    const vehicle = await Vehicle.findByPk(vehicleId, {include: { all: true}});
+    const vehicle = await getVehicle(vehicleId);
 
     if (!vehicle) {
         return res.status(404).json({message: `vehicle '${vehicleId}' not found`});
