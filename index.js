47cb5a09d1329d57ae2beef0b63777c4b46934ac
samael pola <samaelpola06@gmail.com>
2024-11-25 15:20:13 +0100

    fix: middleware, clean code

--- index.js
+++ index.js
@@ -5,50 +5,12 @@ const modelRoutes = require("./src/routes/model.routes");
 const reservationRoutes = require("./src/routes/reservation.routes");
 const vehicleRoutes = require("./src/routes/vehicle.routes");
 const loadModels = require("./src/models/index")
-const morgan = require("morgan");
-const chalk = require("chalk");
 
 const app = express();
 app.use(express.json());
 
 (async () => await loadModels())();
 
-// Apply color to the status code
-// morgan.token("status", (req, res) => {
-//     const status = res.statusCode;
-//     const color =
-//         status >= 500
-//             ? chalk.red
-//             : status >= 400
-//             ? chalk.yellow
-//             : status >= 300
-//             ? chalk.cyan
-//             : status >= 200
-//             ? chalk.green
-//             : chalk.white
-//     ;
-//     return color(status);
-// });
-//
-// // Apply color to the method
-// morgan.token("method", (req, res) => {
-//     const method = req.method;
-//     const color =
-//         method === "GET"
-//             ? chalk.green
-//             : method === "POST"
-//             ? chalk.yellow
-//             : method === "PUT"
-//             ? chalk.cyan
-//             : method === "DELETE"
-//             ? chalk.red
-//             : chalk.white
-//     ;
-//     console.log(chalk.red("totottttttttttttottttttttot"))
-//     return color(method.toString());
-// });
-//
-// app.use(morgan(":method :url - :status"));
 app.use(express.json());
 app.use("/customers", customerRoutes);
 app.use("/models", modelRoutes);
--- src/middlewares/reservation.middleware.js
+++ src/middlewares/reservation.middleware.js
@@ -1,8 +1,8 @@
-const Reservation = require("../models/reservation.model");
+const {getReservation} = require("../services/reservation.service");
 
 const checkReservationExist = async (req, res, next) => {
     const { reservationId } = req.params;
-    const reservation = await Reservation.findByPk(reservationId, {include: { all: true}});
+    const reservation = await getReservation(reservationId);
 
     if (!reservation) {
         return res.status(404).json({message: `reservation '${reservationId}' not found`});
